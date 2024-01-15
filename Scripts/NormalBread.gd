d6f18374ab7c39450a2b5e3358b4ceb801fb3588
vitames404 <vitorames@protonmail.com>
2024-01-15 09:30:04 -0300

    Added first test sprites

---
+++ Scripts/NormalBread.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):
+			Global.whatBread = 1
+			print("You choose the bread: " + str(Global.whatBread))
+			
