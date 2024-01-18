901e4ec9305eeadd15481aef0ca47c8cffd192b5
paginnini <edo.illustrations@gmail.com>
2024-01-18 15:48:26 -0300

    add white bread bag

---
+++ Scripts/whitebag.gd
@@ -0,0 +1,28 @@
+extends AnimatedSprite2D
+
+
+@onready var white_bread = preload("res://white_bread.tscn")
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):
+			Global.whatBread = 1
+	
+			# Acessa a cena principal
+			var main_scene = get_tree().get_root()
+			
+			var check_bread = main_scene.get_node_or_null("bread")
+			if(check_bread):
+				print("PAO EXCLUIDO")
+				check_bread.free()
+			
+			# Instancia o novo objeto da cena do pão
+			var current_bread = white_bread.instantiate()
+			current_bread.name = "bread"
+			print("you choose bread: 1")
+			
+			# Define a posição global do objeto com base na posição do mouse
+			current_bread.global_position =  Vector2(800,300)
+			
+			# Adiciona o novo objeto como filho da cena principal
+			main_scene.add_child(current_bread)
